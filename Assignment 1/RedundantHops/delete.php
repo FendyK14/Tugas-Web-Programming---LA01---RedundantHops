96c1ff2451c8bb5911362ccb8cd0f63896aa4071
Stevanus <48939864+StevanusO@users.noreply.github.com>
2022-10-13 15:14:55 +0700

    Delete Assignment 1/RedundantHops directory

--- Assignment 1/RedundantHops/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require ("connect.php");
-    $nim_to_del = $_GET['request_delete_by_nim'];
-    $my_query = "DELETE FROM mahasiswa WhERE nomor_induk_mhs = '$nim_to_del'";
-
-    mysqli_query($connection, $my_query);
-    header("Location: manage.php");
-?>
